b69622d5e8781ae368b30763e656f510b66feee2
Eric Yeargan <eric@yeargan.org>
2022-02-27 13:35:02 -0600

    Add Teensy4.x support

--- src/BuildOpt.h
+++ src/BuildOpt.h
@@ -734,6 +734,75 @@
     #undef yield
     #endif
 
+  #elif defined(ARDUINO_TEENSY40) || defined(ARDUINO_TEENSY41)
+  // name for your platform
+  #define RADIOLIB_PLATFORM                           "Teensy4"
+
+  // the following parameters must always be defined
+  #define RADIOLIB_PIN_TYPE                           uint8_t
+  #define RADIOLIB_PIN_MODE                           uint8_t
+  #define RADIOLIB_PIN_STATUS                         uint8_t
+  #define RADIOLIB_INTERRUPT_STATUS                   RADIOLIB_PIN_STATUS
+  #define RADIOLIB_DIGITAL_PIN_TO_INTERRUPT(p)        digitalPinToInterrupt(p)
+  #define RADIOLIB_NC                                 (0xFF)
+  #define RADIOLIB_DEFAULT_SPI                        SPI
+  #define RADIOLIB_DEFAULT_SPI_SETTINGS               SPISettings(2000000, MSBFIRST, SPI_MODE0)
+  #define RADIOLIB_NONVOLATILE                        PROGMEM
+  #define RADIOLIB_NONVOLATILE_READ_BYTE(addr)        pgm_read_byte(addr)
+  #define RADIOLIB_TYPE_ALIAS(type, alias)            using alias = type;
+
+  // Arduino API callbacks
+  // the following are signatures of Arduino API functions of the custom platform
+  // for example, pinMode on Arduino Uno is defined as void pinMode(uint8_t pin, uint8_t mode)
+  // all fo the callbacks below are taken from Arduino Uno
+  #define RADIOLIB_CB_ARGS_PIN_MODE                   (void, pinMode, uint8_t pin, uint8_t mode)
+  #define RADIOLIB_CB_ARGS_DIGITAL_WRITE              (void, digitalWrite, uint8_t pin, uint8_t value)
+  #define RADIOLIB_CB_ARGS_DIGITAL_READ               (uint8_t, digitalRead, uint8_t pin)
+  #define RADIOLIB_CB_ARGS_TONE                       (void, tone, uint8_t pin, uint16_t frequency, uint32_t duration)
+  #define RADIOLIB_CB_ARGS_NO_TONE                    (void, noTone, uint8_t _pin)
+  #define RADIOLIB_CB_ARGS_ATTACH_INTERRUPT           (void, attachInterrupt, uint8_t interruptNum, void (*userFunc)(void), int mode)
+  #define RADIOLIB_CB_ARGS_DETACH_INTERRUPT           (void, detachInterrupt, uint8_t interruptNum)
+  #define RADIOLIB_CB_ARGS_YIELD                      (void, yield, void)
+  #define RADIOLIB_CB_ARGS_DELAY                      (void, delay, unsigned long ms)
+  #define RADIOLIB_CB_ARGS_DELAY_MICROSECONDS         (void, delayMicroseconds, uint32_t usec)
+  #define RADIOLIB_CB_ARGS_MILLIS                     (unsigned long, millis, void)
+  #define RADIOLIB_CB_ARGS_MICROS                     (unsigned long, micros, void)
+  #define RADIOLIB_CB_ARGS_SPI_BEGIN                  (void, SPIbegin, void)
+  #define RADIOLIB_CB_ARGS_SPI_BEGIN_TRANSACTION      (void, SPIbeginTransaction, void)
+  #define RADIOLIB_CB_ARGS_SPI_TRANSFER               (uint8_t, SPItransfer, uint8_t b)
+  #define RADIOLIB_CB_ARGS_SPI_END_TRANSACTION        (void, SPIendTransaction, void)
+  #define RADIOLIB_CB_ARGS_SPI_END                    (void, SPIend, void)
+
+  // the following must be defined if the Arduino core does not support tone function
+  //#define RADIOLIB_TONE_UNSUPPORTED
+
+  // some platforms seem to have issues with SPI modules that use a command interface
+  // this can be mitigated by adding delays into SPI communication
+  // (see https://github.com/jgromes/RadioLib/issues/158 for details)
+  //#define RADIOLIB_SPI_SLOWDOWN
+
+  // some of RadioLib drivers may be excluded, to prevent collisions with platforms (or to speed up build process)
+  // the following is a complete list of all possible exclusion macros, uncomment any of them to disable that driver
+  // NOTE: Some of the exclusion macros are dependent on each other. For example, it is not possible to exclude RF69
+  //       while keeping SX1231 (because RF69 is the base class for SX1231). The dependency is always uni-directional,
+  //       so excluding SX1231 and keeping RF69 is valid.
+  //#define RADIOLIB_EXCLUDE_CC1101
+  //#define RADIOLIB_EXCLUDE_NRF24
+  //#define RADIOLIB_EXCLUDE_RF69
+  //#define RADIOLIB_EXCLUDE_SX1231     // dependent on RADIOLIB_EXCLUDE_RF69
+  //#define RADIOLIB_EXCLUDE_SI443X
+  //#define RADIOLIB_EXCLUDE_RFM2X      // dependent on RADIOLIB_EXCLUDE_SI443X
+  //#define RADIOLIB_EXCLUDE_SX127X
+  //#define RADIOLIB_EXCLUDE_RFM9X      // dependent on RADIOLIB_EXCLUDE_SX127X
+  //#define RADIOLIB_EXCLUDE_SX126X
+  //#define RADIOLIB_EXCLUDE_SX128X
+  //#define RADIOLIB_EXCLUDE_AFSK
+  //#define RADIOLIB_EXCLUDE_AX25
+  //#define RADIOLIB_EXCLUDE_HELLSCHREIBER
+  //#define RADIOLIB_EXCLUDE_MORSE
+  //#define RADIOLIB_EXCLUDE_RTTY
+  //#define RADIOLIB_EXCLUDE_SSTV
+
 #elif defined(RASPI)
     // RaspiDuino framework (https://github.com/me-no-dev/RasPiArduino)
     #define RADIOLIB_PLATFORM                           "RasPiArduino"
